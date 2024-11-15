15bea69e4123f6d0ddec43834e949bcc5872cf19
CRISTIAN\CR <crisduban@gmail.com>
2024-11-14 19:05:12 -0500

    ajuste de busqueda

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-inicio',
@@ -29,4 +30,60 @@ export class InicioComponent {
   onMouseLeave() {
     this.isHovered = false; // Cuando el mouse sale del video
   }
+  video() {
+    // Swal.fire({
+    //   title: "<strong>Video Tutorial</strong>",      
+    //   html: `
+    //     <div class="video-container">
+    //       <iframe 
+    //         width="640" 
+    //         height="360" 
+    //         src="https://www.youtube.com/embed/wqy9tYrc6Es?autoplay=1&mute=1&loop=1&playlist=wqy9tYrc6Es&modestbranding=1&showinfo=0&rel=0&controls=0" 
+    //         frameborder="0" 
+    //         allow="autoplay; encrypted-media" 
+    //         allowfullscreen>
+    //       </iframe>
+    //       <div class="overlay"></div>
+    //     </div>
+    //   `,
+    //   showCloseButton: true,
+    //   focusConfirm: false,
+    //   confirmButtonText: `
+    //     <i class="fa fa-thumbs-up"></i> 
+    //   `,
+    //   customClass: {
+    //     htmlContainer: 'custom-swal-html'
+    //   }
+    // });
+    Swal.fire({
+      title: "<strong>¡Bienvenido!</strong>",
+      html: `
+        <div class="video-container">
+          <iframe 
+            width="640" 
+            height="360" 
+            src="https://www.youtube.com/embed/wqy9tYrc6Es?autoplay=1&loop=1&playlist=wqy9tYrc6Es&modestbranding=1&showinfo=0&rel=0&controls=0&cc_load_policy=1" 
+            frameborder="0" 
+            allow="autoplay; encrypted-media" 
+            allowfullscreen>
+          </iframe>
+          <div class="overlay"></div>
+        </div>
+      `,
+      showCloseButton: true,
+      showCancelButton: true,
+      focusConfirm: false,
+      confirmButtonText: `Aceptar`,
+      cancelButtonText: `Cancelar`,
+      customClass: {
+        popup: 'custom-large-swal-popup',
+        title: 'custom-large-swal-title',
+        htmlContainer: 'custom-large-swal-html',
+        confirmButton: 'custom-large-swal-confirm',
+        cancelButton: 'custom-large-swal-cancel'
+      }
+    });
+}
+
+
 }
--- src/app/plano-edificio/plano-edificio.component.ts
+++ src/app/plano-edificio/plano-edificio.component.ts
@@ -1,19 +1,41 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { VideoModalComponent } from './video-modal/video-modal.component';
+import { NgClass, NgFor, NgIf } from '@angular/common';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-plano-edificio',
   standalone: true,
-  imports: [VideoModalComponent],
+  imports: [VideoModalComponent,NgFor,NgIf,NgClass],
   templateUrl: './plano-edificio.component.html',
   styleUrls: ['./plano-edificio.component.css']
 })
+
 export class PlanoEdificioComponent implements OnInit {
+ 
+  @ViewChild('inputRef') inputElement!: ElementRef;
   //videoUrl = 'assets/7019977_Arms_Boy_1280x720.mp4'; // Ruta del video
   videoUrl = 'assets/animacion1.mp4'; // Ruta del video
+  
   isHovered = false;
+  idsalonactive:number =0;
+  listaSalon = [
+    {id:501,active:false,posx:260,posy:40},
+    {id:502,active:false,posx:380,posy:40},
+    {id:503,active:false,posx:330,posy:240},
+    {id:504,active:false,posx:450,posy:240},
+    {id:505,active:false,posx:330,posy:340},
+    {id:506,active:false,posx:520,posy:340}
+  ];
  
-  ngOnInit(): void {}
+  labelBotonMapa :string="Mapa de Ruta de Evacuación";
+  urlMapa = "../../assets/MAPA.jpg";
+  activeMapa = false;
+
+  ngOnInit(): void {    
+
+  }
 
   // Ejemplo de lógica para hacer clic en una salas
   onRoomClick(room: string) {
@@ -34,5 +56,36 @@ onMouseLeave() {
   this.isHovered = false; // Cuando el mouse sale del video
 }
 
+buscar(){
+  const salon1 = this.listaSalon.find(x => x.id == this.idsalonactive);
+  if (salon1) {
+    salon1.active = false;
+  }
+  
+  this.idsalonactive = this.inputElement.nativeElement.value;
+  if(this.listaSalon.find(x=>x.id==this.idsalonactive )!=undefined){
+    Swal.fire({
+      title: "salón - "+this.idsalonactive ,
+      text: "El salón sale marcado con el plano de la universidad ",
+      icon: "success"
+    });
+    const salon = this.listaSalon.find(x => x.id == this.idsalonactive );
+    if (salon) {
+      salon.active = true;
+
+    }
+  }else{
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "El salón no existe",      
+    });
+  }  
+}
+
+mapa(){
+  this.labelBotonMapa =this.labelBotonMapa=="Mapa de Ruta de Evacuación" ?"Mapa de los salones":"Mapa de Ruta de Evacuación";
+  this.activeMapa = this.labelBotonMapa=="Mapa de Ruta de Evacuación" ?false:true;  
+}
     
 }
